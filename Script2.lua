01b22a3a2fbef9550aec846aee95f1e6eaa6b9d7
averyai <78996080+averyai@users.noreply.github.com>
2023-02-18 06:02:28 -0500

    Update Script2.lua

--- Script2.lua
+++ Script2.lua
@@ -36,7 +36,7 @@ local unclaimed = {}
 local errCount = -1
 
 local queueonteleport = (syn and syn.queue_on_teleport) or queue_on_teleport or (fluxus and fluxus.queue_on_teleport)
-queueonteleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/CF-Trail/tzechco-PlsDonateAutofarmBackup/main/casino.lua'))()")
+queueonteleport("loadstring(game:HttpGet('hhttps://raw.githubusercontent.com/averyai/test-script/main/Script2.lua'))()")
 
 if not getgenv().settin then
 	getgenv().settin = {
